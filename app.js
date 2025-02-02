912117951e045f014916017aae2559287689784f
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-02-01 23:04:35 -0800

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }))
+
+const PORT = 3000;
+
+const orders = [];
+
+app.get('/', (req,res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/submit-order', (req, res) => {
+    console.log(req.body);
+    orders.push(req.body);
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
